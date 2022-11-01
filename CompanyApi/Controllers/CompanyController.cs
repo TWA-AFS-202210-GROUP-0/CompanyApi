05a55c0ffdbb081178f9511078d9c752cd8c0cf7
Jie Li <JLi147@slb.com>
2022-11-01 15:59:41 +0800

    feat: get all companies

--- CompanyApi/Controllers/CompanyController.cs
+++ CompanyApi/Controllers/CompanyController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using CompanyApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,9 +15,26 @@ namespace CompanyApi.Controllers
         [HttpPost]
         public ActionResult<Company> AddNewCompany(Company company)
         {
+            if (companies.Exists(p => string.Equals(company.Name, p.Name)))
+            {
+                return new ConflictResult();
+            }
+
             company.CompanyID = Guid.NewGuid().ToString();
             companies.Add(company);
             return new CreatedResult($"/companies/{company.CompanyID}", company);
+        }
+
+        [HttpGet]
+        public List<Company> GetAllCompany()
+        {
+            return companies;
+        }
+
+        [HttpDelete]
+        public void DeleteAllPets()
+        {
+            companies.Clear();
         }
     }
 }
--- CompanyApiTest/Controllers/CompanyControllerTest.cs
+++ CompanyApiTest/Controllers/CompanyControllerTest.cs
@@ -20,9 +20,9 @@ namespace CompanyApiTest.Controllers
             // given
             var application = new WebApplicationFactory<Program>();
             var httpClient = application.CreateClient();
+            await httpClient.DeleteAsync("/companies");
             var company = new Company(name: "SLB");
-            var companyJson = JsonConvert.SerializeObject(company);
-            var postBody = new StringContent(companyJson, Encoding.UTF8, "application/json");
+            var postBody = BuildRequestBody(company);
             // when
             var response = await httpClient.PostAsync("/companies", postBody);
             // then
@@ -30,6 +30,49 @@ namespace CompanyApiTest.Controllers
             var responseBody = await response.Content.ReadAsStringAsync();
             var createdCompany = JsonConvert.DeserializeObject<Company>(responseBody);
             Assert.NotEmpty(createdCompany.CompanyID);
+        }
+
+        [Fact]
+        public async Task Should_add_new_company_failly()
+        {
+            // given
+            var application = new WebApplicationFactory<Program>();
+            var httpClient = application.CreateClient();
+            await httpClient.DeleteAsync("/companies");
+            var company = new Company(name: "SLB");
+            var postBody = BuildRequestBody(company);
+            await httpClient.PostAsync("/companies", postBody);
+            // when
+            var response = await httpClient.PostAsync("/companies", postBody);
+            // then
+            Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_get_all_companies_successfully()
+        {
+            // given
+            var application = new WebApplicationFactory<Program>();
+            var httpClient = application.CreateClient();
+            await httpClient.DeleteAsync("/companies");
+            var slb = new Company(name: "SLB");
+            var postBodySlb = BuildRequestBody(slb);
+            await httpClient.PostAsync("/companies", postBodySlb);
+            var tw = new Company(name: "TW");
+            var postBodyTw = BuildRequestBody(tw);
+            await httpClient.PostAsync("/companies", postBodyTw);
+            // when
+            var response = await httpClient.GetAsync("/companies");
+            // then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        private static StringContent BuildRequestBody(Company company)
+        {
+            company = new Company(name: "SLB");
+            var companyJson = JsonConvert.SerializeObject(company);
+            var postBody = new StringContent(companyJson, Encoding.UTF8, "application/json");
+            return postBody;
         }
     }
 }
