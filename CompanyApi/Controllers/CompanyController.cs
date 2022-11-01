cfc71e73f3098e791d1281d29674884abf8dc8b9
Jie Li <JLi147@slb.com>
2022-11-01 15:34:54 +0800

    feat: add company

---
+++ CompanyApi/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using CompanyApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyApi.Controllers
+{
+    [ApiController]
+    [Route("companies")]
+    public class CompanyController : ControllerBase
+    {
+        private static List<Company> companies = new List<Company>();
+        [HttpPost]
+        public ActionResult<Company> AddNewCompany(Company company)
+        {
+            company.CompanyID = Guid.NewGuid().ToString();
+            companies.Add(company);
+            return new CreatedResult($"/companies/{company.CompanyID}", company);
+        }
+    }
+}
---
+++ CompanyApi/Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CompanyApi.Models
+{
+    public class Company
+    {
+        public Company(string name)
+        {
+            Name = name;
+            CompanyID = string.Empty;
+        }
+
+        public string? CompanyID { get; set; }
+        public string Name { get; set; }
+    }
+}
